084a23dcde7ef5e0699774f8c64875c263d760df
facundojulia11 <facundo.julia@ioet.com>
2024-08-17 15:13:25 -0300

    Add beverage model.

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -22,8 +22,12 @@ class Ingredient(db.Model):
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
 
-
 class Size(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class Beverage(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -31,7 +35,8 @@ class Size(db.Model):
 
 class OrderDetail(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
-    ingredient_price = db.Column(db.Float)
     order_id = db.Column(db.Integer, db.ForeignKey('order._id'))
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient._id'))
     ingredient = db.relationship('Ingredient', backref=db.backref('ingredient'))
+    beverage_id = db.Column(db.Integer, db.ForeignKey('beverage._id'))
+    beverage = db.relationship('Beverage', backref=db.backref('beverage'))
