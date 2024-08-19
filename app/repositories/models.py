88b55b3e5a2368fea79565d56ac34e1baeb0807d
facundojulia11 <facundo.julia@ioet.com>
2024-08-19 04:37:01 -0300

    hot fix: Duplicated model beverage

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -29,12 +29,7 @@ class Beverage(BaseProduct):
 
 class Ingredient(BaseProduct):
     __tablename__="ingredient"
-
-class Beverage(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-
+    
 
 class OrderDetail(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
